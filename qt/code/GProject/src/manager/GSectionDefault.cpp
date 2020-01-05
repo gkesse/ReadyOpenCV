d71c398b83c96bc91515ec2f75eaeefe635efc84
Pierre YURI <pierre.yuri@outlook.fr>
2020-01-05 18:41:18 +0100

    Initial Commit

--- qt/code/GProject/src/manager/GSectionDefault.cpp
+++ qt/code/GProject/src/manager/GSectionDefault.cpp
@@ -7,23 +7,62 @@ GSectionDefault::GSectionDefault(QWidget* parent) :
 GSection(parent) {
 	QVBoxLayout* lMainLayout = new QVBoxLayout;
 
+	QScrollArea* lScrollArea = new QScrollArea;
+	QVBoxLayout* lScrollLayout = new QVBoxLayout;
+	QWidget* lScrollWidget = new QWidget;
+
 	GModule* lModule;
 
 	lModule = GModule::Create("VIDEO");
-	lMainLayout->addWidget(lModule);
+	lScrollLayout->addWidget(lModule);
 
 	lModule = GModule::Create("IMAGE");
-	lMainLayout->addWidget(lModule);
+	lScrollLayout->addWidget(lModule);
 
 	lModule = GModule::Create("VIDEO");
-	lMainLayout->addWidget(lModule);
+	lScrollLayout->addWidget(lModule);
 
 	lModule = GModule::Create("IMAGE");
-	lMainLayout->addWidget(lModule);
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("VIDEO");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("IMAGE");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("VIDEO");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("IMAGE");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("VIDEO");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("IMAGE");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("VIDEO");
+	lScrollLayout->addWidget(lModule);
+
+	lModule = GModule::Create("IMAGE");
+	lScrollLayout->addWidget(lModule);
+
+	lScrollLayout->setMargin(0);
+	lScrollLayout->setSpacing(0);
+	lScrollLayout->setAlignment(Qt::AlignTop);
+
+	lScrollWidget->setLayout(lScrollLayout);
+
+	lScrollArea->setWidget(lScrollWidget);
+	lScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+	lScrollArea->setWidgetResizable(true);
 
 	lMainLayout->setMargin(0);
 	lMainLayout->setSpacing(0);
 	lMainLayout->setAlignment(Qt::AlignTop);
+	lMainLayout->addWidget(lScrollArea);
 
 	setLayout(lMainLayout);
 }
